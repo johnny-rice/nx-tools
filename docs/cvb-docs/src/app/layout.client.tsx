d161f7c247c54dafc327f13ff05e7daf0a9c7759
Gustavo Perdomo <gperdomor@gmail.com>
2026-02-27 19:49:44 -0300

    feat(cvb-docs): updated cvb docs

--- docs/cvb-docs/src/app/layout.client.tsx
+++ docs/cvb-docs/src/app/layout.client.tsx
@@ -2,10 +2,7 @@
 
 import Logo from '@/app/icon.svg';
 import Image from 'next/image';
-import { useId } from 'react';
 
 export function CVBIcon(props: React.SVGProps<SVGSVGElement>) {
-  const id = useId();
-
   return <Image src={Logo} alt="Logo" width={20} height={20} />;
 }
